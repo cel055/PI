0191dc0de36d28aa7f85e472c248459a0d1a7b4f
Marcelo <cel055.ufsc@gmail.com>
2014-11-10 09:37:12 -0200

    eventos na historia

--- codigos/codigo.js
+++ codigos/codigo.js
@@ -2,4 +2,16 @@ $(document).ready(inicia);
 
 function inicia(){
     $("#acordion").accordion();
+    $("#container .quadrado").on("click",abreDiv);
+}
+
+function abreDiv(evento){
+    $("#container .quadradoE").css("display","none");
+    if(this.id == "divPacMonster"){
+        $("#textoPacMonster").css("display","block");
+    }else if(this.id == "divDesenvolvimento"){
+        $("#textoDesenvolvimento").css("display","block");
+    }else{
+        $("#textoEmpresa").css("display","block");
+    }
 }
