d8b8c7694dd1c5a1ef5361b5bfb5766b1519445b
Marcelo <cel055.ufsc@gmail.com>
2014-11-11 12:50:47 -0200

    enviar dois emails e um email resposta e mensagem amigavel na tela do site

--- enviaMail.php
+++ enviaMail.php
@@ -4,7 +4,12 @@
     $assunto = $_POST["assunto"];
     $msg = $_POST["mensagem"];
 
-    $header = implode("\n",array("From: $email", "Subject: teste","Return-Path: $email","MIME-Version: 1.0", "X-Priority: 3","Coontent-Type: text/html; charset=UTF-8"));
-    mail("cel055.ufsc@gmail","TestePHP",$msg,$header);
-    echo"<script>location.href='index.html';</script>";
+    $header = implode("\n",array("From: $email", "Subject: $assunto","Return-Path: $email","MIME-Version: 1.0", "X-Priority: 3","Coontent-Type: text/html; charset=UTF-8"));
+    mail("cel055.ufsc@gmail","$assunto",$msg,$header);
+    mail("herbert.sprengel@gmail.com","$assunto",$msg,$header);
+
+    $header = implode("\n",array("From: cel055.ufsc@gmail.com", "Subject: Equipe PacMonster","Return-Path: cel055.ufsc@gmail.com","MIME-Version: 1.0", "X-Priority: 3","Coontent-Type: text/html; charset=UTF-8"));
+    $msg = "Obrigado pela mensegem.\nEste email foi gerado automaticamente, favor não responder";
+    mail("$email","Equipe PacMonster",$msg,$header);
+    echo"<div style='position:fixed;top:30%;left:30%;z-index:1000'><p>Mensagem enviada com sucesso.\nObrigado.</p><input type='button' value='OK' onclick='function(){location.href=" + '"index.html"' + ";}' /></div>";
 ?>
